8b330e99a0e46308233821371d9e4964892d12af
willian <wils14@hotmail.es>
2019-06-22 13:34:24 -0500

    controlador tablas sin relacionar

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('entidadformadora','TrainingEntityController');
+Route::resource('periodoacademico','AcademicPeriodController');
+Route::resource('role','RoleController');
